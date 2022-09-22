bebbf44c14f2122e1c35bc5a536d5aa9db030f4b
milesi <irina-rxv@yandex.ru>
2022-09-22 22:10:22 +0300

    дз сделал все

---
+++ Program1.py
@@ -0,0 +1,42 @@
+# day = int(input('Введите день недели: '))
+# if day == 6 or day == 7:
+#     print('да')
+# else:
+#     print('нет')
+
+
+# x = int(input('введите ось Х '))
+# y = int(input('введите ось У '))
+# if x != 0 and y != 0:
+#     if x>0 and y>0:
+#         print('первая четверть')
+#     elif x<0 and y>0:
+#         print('вторая четверть')
+#     elif x<0 and y<0:
+#         print('третья четверть')
+#     elif x>0 and y<0:
+#         print('четвертая четверть')
+
+
+# x = [0,0,0]
+# if (not (x[0] or x[1] or x[2])) == (not x[0] and not x[1] and not x[2]):
+#     print('истино')
+# else:
+#     print('ложь')
+
+
+# x = int(input('введите четверть '))
+# if x>0:
+#     if x == 1:
+#         print('x[0;+∞) | y[0;+∞)')
+#     elif x == 2:
+#         print('x(-∞;0] | y [0;+∞)')
+#     elif x == 3:
+#         print('x(-∞;0] | y[0;-∞)')
+#     elif x == 4:
+#         print('x[0;+∞) | y[0;-∞)')
+
+
+# a = [1,5]
+# b = [2,6]
+# print(((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** (0.5))
